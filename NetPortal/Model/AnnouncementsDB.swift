286e61d716d3b5209fa5aca7860c0d3f9c502d94
Siyabonga Zondo <siyabongazondo@Administrators-iMac.local>
2018-02-27 14:42:03 +0200

    first commit

---
+++ NetPortal/Model/AnnouncementsDB.swift
@@ -0,0 +1,9 @@
+//
+//  AnnouncementsDB.swift
+//  NetPortal
+//
+//  Created by Siyabonga Zondo on 2018/02/27.
+//  Copyright © 2018 Siyabonga Zondo. All rights reserved.
+//
+
+import Foundation
---
+++ NetPortal/Networking/LoginSignup.swift
@@ -0,0 +1,9 @@
+//
+//  LoginSignup.swift
+//  NetPortal
+//
+//  Created by Siyabonga Zondo on 2018/02/16.
+//  Copyright © 2018 Siyabonga Zondo. All rights reserved.
+//
+
+import Foundation
